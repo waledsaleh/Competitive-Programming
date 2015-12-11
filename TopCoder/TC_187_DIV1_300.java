a5c9992c2d3ded8bd0079d140637d83327027cdc
waledsaleh <waledsalah2005@gmail.com>
2015-12-11 22:37:21 +0200

    new edit

--- TopCoder/TC_187_DIV1_300.java
+++ TopCoder/TC_187_DIV1_300.java
@@ -13,12 +13,11 @@ public class TC_187_DIV1_300 {
 	}
 	 static String lastCyberword(String cyberline)
 	 {
-		String res="";
-		 cyberline = cyberline.replace("-", "").replaceAll("[^a-zA-Z0-9@\\s]", " ");
-		 
-		 String[] split =cyberline.split(" ");int n =split.length;
-		 for(int i =n-1; i>=0;i--)if(!split[i].equals(" ")){res=split[i];break;}
 		
-		 return res;
+		 cyberline = cyberline.replace("-", "").replaceAll("[^a-zA-Z0-9@]", " ");
+		 
+		 String[] split =cyberline.split(" ");
+		 
+		 return split[split.length-1];
 	 }
 }
